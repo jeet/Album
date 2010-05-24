83229eec1d77c9d90cb33d424f15ce7297b74627
jeet <jeet.ajay@gmail.com>
2010-05-24 19:53:56 +0530

    misc

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,8 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+  def  show_thumbnail(album)
+       image = album.images.rand
+       image_tag image.photo.url(:small)
+     rescue
+  end
 end
